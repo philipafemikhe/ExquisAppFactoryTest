b05ded107ba71c65e9c37cad47146f6a222be9fd
afemikhephilip <phillip.afemikhe@ikooba.com>
2020-07-20 17:55:58 +0100

    first commit

---
+++ src/main/java/com/latecomer/LateComerApp/LateComerAppApplication.java
@@ -0,0 +1,13 @@
+package com.latecomer.LateComerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LateComerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LateComerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/latecomer/LateComerApp/LateComerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.latecomer.LateComerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LateComerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
